9d31d67e70bcf96ba4cef1a8f80864cf4f99646a
Guille Yi <guillermo.yi.ext@macrohealth.com>
2023-11-24 13:55:02 -0300

    uno

---
+++ SorteoTest/Sorteo.Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Sorteo.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+    }
+}
